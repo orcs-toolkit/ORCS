ce2f8467bb3c1ec78be267f9a4efc1acd4248eef
shreyasssk <pauliteshreyas@gmail.com>
2022-01-31 01:00:23 +0530

    converted to ecma script and added logger. Also changed file names

--- socket-server/src/routes/machine/protectedUser.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const requestLogin = require('../../middlewares/requestLogin');
-
-router.get('/api/test', requestLogin, async (req, res) => {
-	res.send('Hello!');
-});
-
-module.exports = router;
