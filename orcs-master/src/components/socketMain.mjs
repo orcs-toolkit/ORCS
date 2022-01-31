71ba63e5ba51090880d0d962d703a5123067f02c
shreyasssk <pauliteshreyas@gmail.com>
2022-01-31 21:18:18 +0530

    feat:added socket logout feature

--- orcs-master/src/components/socketMain.mjs
+++ orcs-master/src/components/socketMain.mjs
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
-import { Machine } from '../models/Machine.mjs';
+import {
+	checkAndAdd,
+	setActiveState,
+	onMachineDisconnect,
+} from '../services/MachineCheckAndAdd.mjs';
 import { Logger } from '../services/logger.mjs';
 
 const logger = new Logger();
@@ -29,31 +33,35 @@ export function socketMain(io, socket) {
 	// console.log('Socket Session: ', socket.request.session);
 
 	socket.on('clientAuth', (key) => {
-		if (key === 'clientAuth') {
+		if (key === 'student') {
 			// valid UI client joined
-			socket.join('ui');
-			logger.info('React client joined!');
-			Machine.find({}, (err, docs) => {
-				docs.forEach((aMachine) => {
-					// on load assume all machines are offline
-					aMachine.isActive = false;
-					io.to('ui').emit('data', aMachine);
-				});
-			});
+			socket.join('student');
+			logger.info(`Student with ID ${socket.id} joined!`);
+			setActiveState(io);
+		} else if (key === 'faculty') {
+			socket.join('faculty');
+			logger.info(`Faculty with ID ${socket.id} joined!`);
+			setActiveState(io);
+		} else if (key === 'admin') {
+			socket.join('admin');
+			logger.info(`Admin with ID ${socket.id} joined!`);
+			setActiveState(io);
 		} else {
 			// invalid client
 			socket.disconnect(true);
 		}
 	});
 
+	socket.on('logout', async () => {
+		var room = Array.from(socket.rooms);
+		await socket.leave(room[1]);
+		logger.warn(`Leaving room: ${room[1]}`);
+		logger.warn(`Client with socket id: ${socket.id} has left the room!`);
+	});
+
 	socket.on('disconnect', () => {
-		Machine.find({ macA }, (err, docs) => {
-			if (docs.length > 0) {
-				// send one last emit to client
-				docs[0].isActive = false;
-				io.to('ui').emit('data', docs[0]);
-			}
-		});
+		onMachineDisconnect(macA, io);
+		logger.warn(`Client with socket id: ${socket.id} has disconnected!`);
 	});
 
 	socket.on('initPerfData', async (data) => {
@@ -63,25 +71,6 @@ export function socketMain(io, socket) {
 	});
 
 	socket.on('perfData', (data) => {
-		io.to('ui').emit('data', data);
-	});
-}
-
-function checkAndAdd(data) {
-	return new Promise((resolve, reject) => {
-		Machine.findOne({ macA: data.macA }, (err, docs) => {
-			if (err) {
-				throw err;
-				// reject(err);
-			} else if (docs === null) {
-				// record or machine not added to db, so add it!
-				let newMachine = new Machine(data);
-				newMachine.save();
-				resolve('Machine registered!');
-			} else {
-				// record or machine already added to db, so resolve
-				resolve('Machine already registered!');
-			}
-		});
+		io.to('admin').emit('data', data);
 	});
 }
