da612d68447aa2f90a24cf35b9917e5d313c3097
shreyasssk <pauliteshreyas@gmail.com>
2022-01-31 23:11:00 +0530

    feat:added system-client basic

---
+++ node-exporter/system-client/components/Layout.js
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Footer from './Footer';
+
+export default function Layout({ children }) {
+	return (
+		<div>
+			<Head>
+				<title>ORCS System Client</title>
+				<meta name="description" content="ORCS - System Surveillence Tool" />
+				<link rel="icon" href="/favicon.ico" />
+
+				<link
+					href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+					rel="stylesheet"
+				/>
+
+				<link
+					href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+					rel="stylesheet"
+				/>
+
+				<link
+					href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.css"
+					rel="stylesheet"
+				/>
+
+				<script
+					type="text/javascript"
+					src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"
+				></script>
+			</Head>
+
+			<div> {children} </div>
+
+			<Footer />
+		</div>
+	);
+}
