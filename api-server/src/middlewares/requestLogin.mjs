59729777d8632923953e837bb3c196e44b151712
shreyasssk <pauliteshreyas@gmail.com>
2022-04-14 21:19:16 +0530

    rectified requestLogin middleware

--- api-server/src/middlewares/requestLogin.mjs
+++ api-server/src/middlewares/requestLogin.mjs
@@ -1,5 +1,7 @@
 export const requestLogin = (req, res, next) => {
-	if (!req.session.user) {
+	var header = req.headers.authorization || '';
+	var token = header.split(/\s+/).pop() || '';
+	if (!header && !token) {
 		return res.status(401).send({
 			success: false,
 			message: 'Please login to access this info.',
--- api-server/src/routes/policy/index.mjs
+++ api-server/src/routes/policy/index.mjs
@@ -9,23 +9,17 @@ import { roleBasedPolicyRouter } from './getPolicyByRole.mjs';
 import { roleWisePolicyRouter } from './getRoleWisePolicy.mjs';
 import { updateSinglePolicyRouter } from './updateSinglePolicy.mjs';
 
+import { requestLogin } from '../../middlewares/requestLogin.mjs';
+
 const router = express.Router();
 
 router.use('/getPolicies', getAllPoliciesRouter);
 router.use('/getPolicy', getSinglePolicyRouter);
-router.use('/updatePolicy', updatePolicyRouter);
+router.use('/updatePolicy', requestLogin, updatePolicyRouter);
 router.use('/setPolicy', setPolicyRouter);
 router.use('/deletePolicy', deletePolicyRouter);
 router.use('/getRolePolicy', roleBasedPolicyRouter);
 router.use('/getRoleWisePolicy', roleWisePolicyRouter);
 router.use('/updateSinglePolicy', updateSinglePolicyRouter);
-
-router.get('/test', async (req, res) => {
-	var data = getAllPoliciesExample();
-	data.then((v) => {
-		console.log('Data from function call', v);
-		res.json(v);
-	});
-});
 
 export { router as policyRouter };
